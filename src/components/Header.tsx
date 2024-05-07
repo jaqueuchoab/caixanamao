308ade93b36c1f68c423de183a646df786a06458
jaqueuchoab <jaqueline.uchoab@gmail.com>
2024-05-07 20:08:41 -0300

    startscreen-page
    feat: header na page de apresentação

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import logoLight from '../assets/logos/light-theme-assets/cnm-logo-light.png'; 
-import iconModeLight from '../assets/icons/icon-mode-light.png';
-import iconMenuHamburguer from '../assets/icons/icon-hamburguer-light.png';
+import headerStyle from '../styles/Header.module.css';
+import logoLight from '../assets/logos/light-theme-assets/cnm-logo-light.svg'; 
+import iconModeLight from '../assets/icons/icon-mode-light.svg';
+import iconMenuHamburguer from '../assets/icons/icon-hamburguer-light.svgicon-mode-light.svg';
 
 const Header = () => {
   return (
-    <header>
-      <img src={logoLight} alt='logo-mode-light'/>
-      <img src={iconModeLight} alt='icon-mode-light'/>
-      <img src={iconMenuHamburguer} alt='icon-menu-hamburguer-light'/>
+    <header className={headerStyle.header}>
+      <img src={logoLight} alt='logo-mode-light' className={headerStyle.logoLight}/>
+      <nav className={headerStyle.nav}>
+        <img src={iconModeLight} alt='icon-mode-light'/>
+        <img src={iconMenuHamburguer} alt='icon-menu-hamburguer-light'/>
+      </nav>
     </header>
   );
 }
