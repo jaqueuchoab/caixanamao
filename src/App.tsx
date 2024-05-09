25e551245e268f3c38701b68ad61b59162063c2a
jaqueuchoab <jaqueline.uchoab@gmail.com>
2024-05-09 00:10:17 -0300

    feat: finalizando header e iniciando configuração de rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import Header from "./components/Header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./views/Home";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <div className="app">
-        <Header/>
+        <BrowserRouter>
+          <Header/>
+          <Routes>
+            <Route path="/" element = {<Home/>}></Route>
+          </Routes>
+          <Footer/>
+        </BrowserRouter>
       </div>
     </>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,19 @@
+// Importando style css
 import headerStyle from '../styles/Header.module.css';
+// Importando imagens
 import logoLight from '../assets/logos/light-theme-assets/cnm-logo-light.svg'; 
 import iconModeLight from '../assets/icons/icon-mode-light.svg';
 import iconMenuHamburguer from '../assets/icons/icon-hamburguer-light.svg';
+// Importando ferramentas para configuração de rotas
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <header className={headerStyle.header}>
-      <img src={logoLight} alt='logo-mode-light' className={headerStyle.logoLight}/>
+      <Link to="/"><img src={logoLight} alt='logo-mode-light' className={headerStyle.logoLight}/></Link>
       <nav className={headerStyle.nav}>
-        <img src={iconModeLight} alt='icon-mode-light'/>
-        <img src={iconMenuHamburguer} alt='icon-menu-hamburguer-light'/>
+        <button><img src={iconModeLight} alt='icon-mode-light'/></button>
+        <button><img src={iconMenuHamburguer} alt='icon-menu-hamburguer-light'/></button>
       </nav>
     </header>
   );
