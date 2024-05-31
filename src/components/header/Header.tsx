78b1e9e311a206148f40b74f01b1853fca6e6e30
jaqueuchoab <jaqueline.uchoab@gmail.com>
2024-05-31 19:35:14 -0300

    feat: Lógica de modos

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,25 +2,36 @@ import React from 'react';
 // Importando style css
 import style from './Header.module.css';
 // Importando imagens
-import cnm_logo_light from '../../assets/logos/light-theme-assets/cnm-logo-light.svg'; 
+import cnm_logo_light from '../../assets/logos/light-theme-assets/cnm-logo-light.svg';
 import icon_mode_light from '../../assets/icons/icon-mode-light.svg';
+import icon_mode_dark from '../../assets/icons/icon-mode-dark.svg';
 import icon_hamburguer_light from '../../assets/icons/icon-hamburguer-light.svg';
 // Importando ferramentas para configuração de rotas
 import { Link } from 'react-router-dom';
 import { useMode } from '../../context/ModeContext';
 
 const Header = () => {
-  const { setMode } = useMode();
+  const { mode, setMode } = useMode();
 
   return (
     <header className={style.header}>
-      <Link to="/"><img src={cnm_logo_light} alt='logo-mode-light' className={style.logoLight}/></Link>
+      <Link to="/">
+        <img
+          src={cnm_logo_light}
+          alt="logo-mode-light"
+          className={style.logoLight}
+        />
+      </Link>
       <nav className={style.nav}>
-        <button onClick={() => setMode('dark')}><img src={icon_mode_light} alt='icon-mode-light'/></button>
-        <button><img src={icon_hamburguer_light} alt='icon-menu-hamburguer-light'/></button>
+        <button onClick={() => setMode(mode === 'light' ? 'dark' : 'light')}>
+          <img src={mode === 'light' ? icon_mode_light : icon_mode_dark} alt={`icon-mode-${mode}`} />
+        </button>
+        <button>
+          <img src={icon_hamburguer_light} alt="icon-menu-hamburguer-light" />
+        </button>
       </nav>
     </header>
   );
-}
+};
 
 export default Header;
