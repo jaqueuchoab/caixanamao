6dd563bc0f2942a08bf9585b7d0a4ee553b2bac0
jaqueuchoab <jaqueline.uchoab@gmail.com>
2024-05-31 22:41:48 -0300

    fix: Home/Apresentação padding

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -17,12 +17,21 @@ import { useMode } from '../../context/ModeContext';
 
 function setColorApp(mode: string) {
   const app: HTMLDivElement | null = document.querySelector('.app');
-  const head: HTMLMetaElement | null = document.querySelector('meta[name="theme-color"]');
+  const themeColor: HTMLMetaElement | null = document.querySelector(
+    'meta[name="theme-color"]',
+  );
+  const themeColorApple: HTMLMetaElement | null = document.querySelector(
+    'meta[name="apple-mobile-web-app-status-bar-style"]',
+  );
 
-  if (app && head) {
+  if (app && themeColor && themeColorApple) {
     mode === 'light'
-      ? (app.style.background = 'var(--color-bg-800)') && (head.content = 'var(--color-bg-800)')
-      : (app.style.background = 'var(--color-bg-100)') && (head.content = 'var(--color-bg-100)');
+      ? (app.style.background = 'var(--color-bg-800)') &&
+        (themeColor.content = 'var(--color-bg-800)') &&
+        (themeColorApple.content = 'var(--color-bg-800)')
+      : (app.style.background = 'var(--color-bg-100)') &&
+        (themeColor.content = 'var(--color-bg-100)') &&
+        (themeColorApple.content = 'var(--color-bg-100)');
   } else {
     throw new Error('app não existe');
   }
