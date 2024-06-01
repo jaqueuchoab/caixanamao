5071a0b1dc22cebe993b373111488fac3f616e1f
jaqueuchoab <jaqueline.uchoab@gmail.com>
2024-05-31 21:15:03 -0300

    feat: Footer modos

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,15 +1,33 @@
 import React from 'react';
+
 // Importando style css
 import style from './Footer.module.css';
+
+//Importando imagens
 import logo_vert_light from '../../assets/logos/light-theme-assets/cnm-logovert-light.svg'
+import logo_vert_dark from '../../assets/logos/dark-theme-assets/cnm-logovert-dark.svg'
+
 // Importando ferramentas para configuração de rotas
 import { Link } from 'react-router-dom';
+import { useMode } from '../../context/ModeContext';
+
+const FooterLight: React.CSSProperties = {
+  background: 'var(--color-bg-100)',
+  color: 'var(--color-bg-600)'
+}
+
+const FooterDark: React.CSSProperties = {
+  background: 'var(--color-bg-800)',
+  color: 'var(--color-bg-300)'
+}
 
 const Footer = () => {
+  const { mode } = useMode();
+
   return (
-    <footer>
+    <footer style={mode === 'light' ? FooterLight : FooterDark}>
       <div className={style.contentFooter}>
-        <img src={logo_vert_light} alt='logo-mode-light' className={style.logoVertLight}/>
+        <img src={mode === 'light' ? logo_vert_light : logo_vert_dark} alt={`logo-mode-${mode}`} className={style.logoVert}/>
         <div className={style.linkActions}>
           <Link to="/login-registration"><p>Login / Cadastro</p></Link>
           <Link to="/benefits"><p>Vantagens</p></Link>
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+
 // Importando style css
 import style from './Header.module.css';
+
 // Importando imagens
 import cnm_logo_light from '../../assets/logos/light-theme-assets/cnm-logo-light.svg';
+import cnm_logo_dark from '../../assets/logos/dark-theme-assets/cnm-logo-dark.svg';
 import icon_mode_light from '../../assets/icons/icon-mode-light.svg';
 import icon_mode_dark from '../../assets/icons/icon-mode-dark.svg';
 import icon_hamburguer_light from '../../assets/icons/icon-hamburguer-light.svg';
+import icon_hamburguer_dark from '../../assets/icons/icon-hamburguer-dark.svg';
+
 // Importando ferramentas para configuração de rotas
 import { Link } from 'react-router-dom';
 import { useMode } from '../../context/ModeContext';
@@ -17,9 +22,9 @@ const Header = () => {
     <header className={style.header}>
       <Link to="/">
         <img
-          src={cnm_logo_light}
-          alt="logo-mode-light"
-          className={style.logoLight}
+          src={mode === 'light' ? cnm_logo_light : cnm_logo_dark}
+          alt={`logo-mode-${mode}`}
+          className={style.logoHeader}
         />
       </Link>
       <nav className={style.nav}>
@@ -27,7 +32,7 @@ const Header = () => {
           <img src={mode === 'light' ? icon_mode_light : icon_mode_dark} alt={`icon-mode-${mode}`} />
         </button>
         <button>
-          <img src={icon_hamburguer_light} alt="icon-menu-hamburguer-light" />
+          <img src={mode === 'light' ? icon_hamburguer_light : icon_hamburguer_dark} alt="icon-menu-hamburguer-light" />
         </button>
       </nav>
     </header>
