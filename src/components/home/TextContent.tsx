ea14f49ccd3f15516804ae0d76f17754d2fc93fd
jaqueuchoab <jaqueline.uchoab@gmail.com>
2024-05-31 21:23:10 -0300

    feat: TextContent modos

--- src/components/home/TextContent.tsx
+++ src/components/home/TextContent.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import style from './TextContent.module.css';
+import { useMode } from '../../context/ModeContext';
 
 // Tipo que define as propriedades que TextContent pode receber
 type Content = {
@@ -8,11 +9,25 @@ type Content = {
 };
 
 function TextContent({ titulo, texto }: Content) {
-  // h3 e o p receberão seus dados por meio de props
+  const { mode } = useMode();
+
   return (
     <div className={style.textContent}>
-      <h3 className={style.h3}>{titulo}</h3>
-      <p className={style.p}>{texto}</p>
+      <h3
+        style={
+          mode === 'light'
+            ? { color: 'var(--color-bg-950)' }
+            : { color: 'var(--color-bg-100)' }
+        }
+        className={style.h3}
+      >
+        {titulo}
+      </h3>
+      <p style={
+          mode === 'light'
+            ? { color: 'var(--color-bg-600)' }
+            : { color: 'var(--color-bg-300)' }
+        }className={style.p}>{texto}</p>
     </div>
   );
 }
