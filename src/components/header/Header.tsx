7acfddf6112ce3582ba0799396d6e2e9fb2e6f9b
jaqueuchoab <jaqueline.uchoab@gmail.com>
2024-06-18 21:42:23 -0300

    fix: Header icons

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -6,10 +6,7 @@ import style from './Header.module.css';
 // Importando imagens
 import cnm_logo_light from '../../assets/logos/light-theme-assets/cnm-logo-light.svg';
 import cnm_logo_dark from '../../assets/logos/dark-theme-assets/cnm-logo-dark.svg';
-import icon_mode_light from '../../assets/icons/icon-mode-light.svg';
-import icon_mode_dark from '../../assets/icons/icon-mode-dark.svg';
-import icon_hamburguer_light from '../../assets/icons/icon-hamburguer-light.svg';
-import icon_hamburguer_dark from '../../assets/icons/icon-hamburguer-dark.svg';
+import CircleHalf from 'phosphor-react/src/icons/CircleHalf';
 
 // Importando ferramentas para configuração de rotas
 import { Link } from 'react-router-dom';
@@ -66,18 +63,7 @@ const Header = () => {
             setMode(mode === 'light' ? 'dark' : 'light');
           }}
         >
-          <img
-            src={mode === 'light' ? icon_mode_light : icon_mode_dark}
-            alt={`icon-mode-${mode}`}
-          />
-        </button>
-        <button>
-          <img
-            src={
-              mode === 'light' ? icon_hamburguer_light : icon_hamburguer_dark
-            }
-            alt="icon-menu-hamburguer-light"
-          />
+          <CircleHalf color={mode === 'light' ? '#080808' : '#FEFEFE'} size={38} />
         </button>
       </nav>
     </header>
