ada246bf4f87744e5805ccf8632e41e18a492f4c
jaqueuchoab <jaqueline.uchoab@gmail.com>
2024-07-05 20:06:56 -0300

    feat: validação de email

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import style from '../input/Input.module.css';
 import { useMode } from '../../context/ModeContext';
 
@@ -8,27 +8,46 @@ type InputProps = {
   type: string;
   setValue: React.Dispatch<React.SetStateAction<string>>;
   placeholder: string;
-  error: null | string; 
+  error: null | string;
   onChange: (target: HTMLInputElement) => void;
   onBlur: (value: string | number) => boolean;
 };
 
-const Input = ({ id, value, type, setValue, placeholder, error, onChange, onBlur }: InputProps) => {
+const Input = ({
+  id,
+  value,
+  type,
+  setValue,
+  placeholder,
+  error,
+  onChange,
+  onBlur,
+}: InputProps) => {
   const { mode } = useMode();
-console.log(value);
+
+  function errorConfig(error: string, className: string, mode: string) {
+    const input = document.querySelector<HTMLInputElement>(className);
+    if (input) input.style.border = `1.4px solid var(--error-${mode})`;
+    return <span style={{color: `var(--error-${mode})`}}>{error}</span>;
+  }
 
   return (
-    <div className={style.inputContainer} id={style[mode] + ' ' + error ? style.error : ''}>
-      <input
-        id={id}
-        type={type}
-        value={value}
-        autoComplete='off'
-        className={style.input}
-        placeholder={placeholder}
-        onChange={({target}) => onChange(target)}
-        onBlur={({target}) => onBlur(target.value)}
-      />
+    <div>
+      <div id={style[mode]} className={style.inputContainer}>
+        <input
+          id={id}
+          type={type}
+          value={value}
+          autoComplete="off"
+          className={style.input}
+          placeholder={placeholder}
+          onChange={({ target }) => onChange(target)}
+          onBlur={({ target }) => onBlur(target.value)}
+        />
+      </div>
+      {error && error?.length !== 0
+          ? errorConfig(error, style.inputContainer, mode)
+          : null}
     </div>
   );
 };
--- src/hooks/useForm.tsx
+++ src/hooks/useForm.tsx
@@ -1,56 +1,56 @@
 import React from 'react';
 
 type typesInput = {
-  [key:string]: {
+  [key: string]: {
     regex: RegExp;
     message: string;
-  }
-}
+  };
+};
 
-const types: typesInput= {
+const types: typesInput = {
   email: {
-    regex: /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i,
-    message: 'Preencha um email válido',
+    regex: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+    message: 'Por favor, insira um email válido.',
   },
   number: {
     regex: /^\d+$/,
     message: 'Apenas números são aceitos.',
-  }
-}
+  },
+};
 
 const useForm = (type: string) => {
   const [value, setValue] = React.useState('');
   const [error, setError] = React.useState<null | string>(null);
 
   function validate(value: string | number) {
-    if(!type) return true;
-    if(typeof value === 'string' && value.length === 0 || !value) {
+    if (!type) return true;
+    if ((typeof value === 'string' && value.length === 0) || !value) {
       setError('Preencha um valor.');
       return false;
-    } else if(types[type] && !types[type].regex.test(value.toString())) {
-        setError(types[type].message);
-        return false;
+    } else if (types[type] && !types[type].regex.test(value.toString())) {
+      console.log(types[type].message, value);
+
+      setError(types[type].message);
+      return false;
     } else {
       setError(null);
       return true;
     }
   }
 
-  function onChange(target:HTMLInputElement) {
-    if(error) validate(target.value);
+  function onChange(target: HTMLInputElement) {
+    if (error) validate(target.value);
     setValue(target.value);
   }
 
-  return (
-    {
-      value, 
-      setValue,
-      onChange,
-      error,
-      validate: () => validate(value),
-      onBlur: () => validate(value),
-    }
-  )
-}
+  return {
+    value,
+    setValue,
+    onChange,
+    error,
+    validate: () => validate(value),
+    onBlur: () => validate(value),
+  };
+};
 
 export default useForm;
