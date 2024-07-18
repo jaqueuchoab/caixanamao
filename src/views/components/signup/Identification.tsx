de0d32a0680453c74844cadbbe13b0f40ca77d11
jaqueuchoab <jaqueline.uchoab@gmail.com>
2024-07-17 22:53:46 -0300

    feat: page cadastro

--- src/views/components/signup/Identification.tsx
+++ src/views/components/signup/Identification.tsx
@@ -5,8 +5,31 @@ import style from './styles/Identification.module.css';
 const Identification = () => {
   return (
     <section className={style.mainContent}>
-      <section>
+      <section className={style.forms}>
         <h3>Cadastrar</h3>
+        <p>Insira seus dados e rapidamente acesse a ferramenta.</p>
+        <div className={style.progressIndicator}>
+          <div className={style.containerProgress}>
+            <div className={style.progressDot}>
+              <p className={style.numberDot}>
+                1
+              </p>
+            </div>
+            <div className={style.indicatorStep}>
+              Identificação
+            </div>
+          </div>
+          <div className={style.lineProgress}>
+            <div></div>
+          </div>
+          <div className={style.containerProgress}>
+            <div className={style.progressDot}>
+              <p className={style.progressDot}>
+                2
+              </p>
+            </div>
+          </div>
+        </div>
       </section>
       <Button>Continuar</Button>
     </section>
