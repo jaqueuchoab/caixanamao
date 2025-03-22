6b42e7fe5d094e97f7d672a294dd360e2659411a
1manuelc <manuelwn21@gmail.com>
2025-03-22 15:36:03 -0300

    refactor: estilos e responsividade do Fallback + artwork

--- src/views/components/fallback/Fallback.tsx
+++ src/views/components/fallback/Fallback.tsx
@@ -1,49 +1,36 @@
-import style from './Fallback.module.css';
-import { Link, useLocation } from 'react-router-dom';
+import { LinkBreak } from '@phosphor-icons/react';
+import { useLocation } from 'react-router-dom';
 import { useMode } from '../../context/ModeContext';
-import { LinkBreakIcon } from '@phosphor-icons/react';
+import {
+	ArtworkContainer,
+	CustomLink,
+	ErrorBadge,
+	FallbackContainer,
+	FallbackContent,
+} from './Fallback.styles';
+import artworkPath from '../../assets/FallbackArtwork.png';
 
 const Fallback = () => {
-	const { mode } = useMode();
+	const { theme } = useMode();
+
 	const { search } = useLocation();
 	const params = new URLSearchParams(search);
 	const errorMessage = params.get('error') || 'Unknown error';
 
-	/*teste*/
-
 	return (
-		<div className={style.fallback} id={style[mode]}>
-			<LinkBreakIcon
-				color={
-					mode === 'light'
-						? 'var(--color-neutral-600)'
-						: 'var(--color-neutral-300)'
-				}
-				size={120}
-			/>
-			<p className={style.p}>Ops... Página não encontrada.</p>
-			<span className={style.span}>
-				<p>{errorMessage}</p>
-			</span>
-			<Link
-				to={'/'}
-				style={
-					mode === 'light'
-						? {
-								color: 'var(--color-green-900)',
-								fontSize: 'var(--size-2xs)',
-								textDecoration: 'underline',
-						  }
-						: {
-								color: 'var(--color-green-300)',
-								fontSize: 'var(--size-2xs)',
-								textDecoration: 'underline',
-						  }
-				}
-			>
-				Voltar para a página anterior
-			</Link>
-		</div>
+		<FallbackContainer theme={theme}>
+			<FallbackContent>
+				<LinkBreak color={theme.colors.texts.secondary} size={64} />
+				<h3>Ops, página não encontrada!</h3>
+				<ErrorBadge theme={theme}>{errorMessage}</ErrorBadge>
+				<CustomLink theme={theme} to={''} onClick={() => window.history.back()}>
+					Voltar para pagina anterior
+				</CustomLink>
+			</FallbackContent>
+			<ArtworkContainer>
+				<img id='fallbackArtwork' src={artworkPath} />
+			</ArtworkContainer>
+		</FallbackContainer>
 	);
 };
 
