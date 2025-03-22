2661997426eca2bdd5b15c65bef7858fed8f040a
1manuelc <manuelwn21@gmail.com>
2025-03-22 14:15:30 -0300

    refactor: DashboardHome refatorado com styled components

--- src/views/components/dashboard/DashboardHome.tsx
+++ src/views/components/dashboard/DashboardHome.tsx
@@ -1,36 +1,11 @@
-import style from './Dashboard.module.css';
-import { useMode } from '../../context/ModeContext';
-import { List, House, Notepad, Plus, FileText, ChartBar } from '@phosphor-icons/react';
+import { Sidebar } from '../sidebar/Sidebar';
+import { DashboardContainer } from './Dashboard.styles';
 
-
-const DashboardHome = () => {
-  const { mode } = useMode();
-
-  return (
-    <div className={style.dashboardContainer} id={style[mode]}>
-      <header className={style.header}>
-        <div className={style.profile}>
-          <div className={style.photo}></div>
-          <div className={style.profileInfos}>
-            <h3 className={style.userName}  style={{color: `var(--primary-${mode}-text)`}}>Nome do usuário</h3>
-            <p className={style.userOcupation} style={{color: `var(--${mode}-highlight)`}}>Cargo</p>
-          </div>
-        </div>
-        <List size={32} color={`var(--icon-${mode}-default)`} />
-      </header>
-
-      {/*Rotas aqui no meio :) */}
-
-      <footer className={style.bottomMenu}>
-        <button className={style.home}><House size={32} /></button>
-        <button className={style.registers}><Notepad size={32} /></button>
-        <button className={style.newRegisters}><Plus size={32} /></button>
-        <button className={style.reports}><FileText size={32} /></button>
-        <button className={style.analysis}><ChartBar size={32} /></button>
-      </footer>
-    </div>
-  );
+export const DashboardHome = () => {
+	return (
+		<DashboardContainer>
+			<Sidebar />
+			{/*Rotas aqui no meio :) */}
+		</DashboardContainer>
+	);
 };
-
-// adaptar as cores para o modo dark
-export default DashboardHome;
