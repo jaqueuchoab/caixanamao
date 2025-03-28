c05d015a31d8fff69c8dc97beccef685f97f9de5
1manuelc <manuelwn21@gmail.com>
2025-03-28 15:43:37 -0300

    feat: container de conteúdo principal na página Dashboard

--- src/views/components/dashboard/Dashboard.styles.ts
+++ src/views/components/dashboard/Dashboard.styles.ts
@@ -1,10 +1,26 @@
 import { styled } from 'styled-components';
+import { ThemeType } from '../../themes/themeType';
 
 export const DashboardContainer = styled.div`
-	display: flex;
-	flex-direction: column;
-	justify-content: space-between;
-	align-items: self-start;
 	height: 100vh;
 	width: 100vw;
+
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+	align-items: self-start;
+`;
+
+export const DashboardContent = styled.main<{ theme: ThemeType }>`
+	height: 100vh;
+	width: 100%;
+	padding: 0 24px;
+
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	gap: 24px;
+
+	background-color: ${(props) => props.theme.colors.backgrounds.dashboard};
 `;
--- src/views/components/dashboard/DashboardHome.tsx
+++ src/views/components/dashboard/DashboardHome.tsx
@@ -1,10 +1,37 @@
+import { Button } from '../button/Button';
 import { Sidebar } from '../sidebar/Sidebar';
-import { DashboardContainer } from './Dashboard.styles';
+import { DashboardContainer, DashboardContent } from './Dashboard.styles';
+import { useTheme } from '../../hooks/useTheme';
+import {
+	CastleTurret,
+	Hash,
+	Link,
+	WarningOctagon,
+} from '@phosphor-icons/react';
 
 export const DashboardHome = () => {
+	const theme = useTheme();
 	return (
 		<DashboardContainer>
 			<Sidebar />
+			<DashboardContent theme={theme}>
+				<Button variant='primary'>
+					<Hash size={24} />
+					Primary
+				</Button>
+				<Button variant='danger'>
+					<WarningOctagon size={24} />
+					Danger
+				</Button>
+				<Button variant='admin'>
+					<CastleTurret size={24} />
+					Admin
+				</Button>
+				<Button variant='link'>
+					<Link size={24} />
+					Link
+				</Button>
+			</DashboardContent>
 			{/*Rotas aqui no meio :) */}
 		</DashboardContainer>
 	);
