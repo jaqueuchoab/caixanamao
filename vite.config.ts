0f4c7821c14355b03138c1123e411d681240d4d3
1manuelc <manuelwn21@gmail.com>
2025-03-28 23:20:08 -0300

    feat: source maps ativados no vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  ssr: {
-    // Add npm packages containing invalid code here
-    noExternal: ['some-library']
-  }
-})
+	plugins: [react()],
+	ssr: {
+		// Add npm packages containing invalid code here
+		noExternal: ['some-library'],
+	},
+	build: {
+		sourcemap: true,
+	},
+});
