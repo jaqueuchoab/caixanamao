e45db45571b0f7697571b871b8ad48d5e6e69498
1manuelc <manuelwn21@gmail.com>
2025-03-31 10:10:09 -0300

    feat: adicionado Jest e React Testing Library para testes em componentes + teste básico em Button

---
+++ src/tests/render-with-theme.tsx
@@ -0,0 +1,16 @@
+import { render, RenderOptions } from '@testing-library/react';
+import { ThemeProvider } from 'styled-components';
+import { darkTheme } from '../views/themes/dark';
+
+// por padrao os testes sao feitos com darkMode, caso deseje, mude aqui
+const AllTheProviders = ({ children }: { children: React.ReactNode }) => {
+	return <ThemeProvider theme={darkTheme}>{children}</ThemeProvider>;
+};
+
+// exporta a funcao render novamente, mas com os wrappers necessarios para o conteudo
+const renderWithTheme = (
+	ui: React.ReactNode,
+	options?: Omit<RenderOptions, 'wrapper'>
+) => render(ui, { wrapper: AllTheProviders, ...options });
+
+export { renderWithTheme };
