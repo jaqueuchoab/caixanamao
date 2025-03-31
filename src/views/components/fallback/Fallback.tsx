e9ed350483da134877374b1d507e276ea5ae107b
1manuelc <manuelwn21@gmail.com>
2025-03-31 12:20:19 -0300

    fix: utilização de Button invés de nova estilização

--- src/views/components/fallback/Fallback.tsx
+++ src/views/components/fallback/Fallback.tsx
@@ -3,12 +3,12 @@ import { useLocation } from 'react-router-dom';
 import { useTheme } from '../../context/ThemeContext';
 import {
 	ArtworkContainer,
-	CustomLink,
 	ErrorBadge,
 	FallbackContainer,
 	FallbackContent,
 } from './Fallback.styles';
 import artworkPath from '../../assets/FallbackArtwork.png';
+import { Button } from '../button/Button';
 
 const Fallback = () => {
 	const { theme } = useTheme();
@@ -23,9 +23,9 @@ const Fallback = () => {
 				<LinkBreak color={theme.colors.texts.secondary} size={64} />
 				<h3>Ops, página não encontrada!</h3>
 				<ErrorBadge>{errorMessage}</ErrorBadge>
-				<CustomLink to={''} onClick={() => window.history.back()}>
+				<Button variant='link' onClick={() => window.history.back()}>
 					Voltar para pagina anterior
-				</CustomLink>
+				</Button>
 			</FallbackContent>
 			<ArtworkContainer>
 				<img id='fallbackArtwork' src={artworkPath} />
