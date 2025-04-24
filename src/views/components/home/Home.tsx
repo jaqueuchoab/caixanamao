d91e669684a7b68b0c515350a90cff64fd015889
jaqueuchoab <jaqueline.uchoab@gmail.com>
2025-04-24 15:31:04 -0300

    Carousel Finished

--- src/views/components/home/Home.tsx
+++ src/views/components/home/Home.tsx
@@ -15,78 +15,72 @@ import style from './styles/Home.module.css';
 import { useMode } from '../../context/ModeContext.tsx';
 
 const Home = () => {
-	const { mode } = useMode();
+  const { mode } = useMode();
 
-	return (
-		<div className={style.mainContent} id={style[mode]}>
-			<Header />
-			<Carousel
-				imagesSrc={[
-					cnm_test_carousel,
-					dark_logo,
-					light_logo,
-					cnm_gradient_arrow,
-					cnm_test_carousel,
-					dark_logo,
-					light_logo,
-					cnm_gradient_arrow,
-				]}
-				options={{ autoplay: { enabled: true, delay: 2500 } }}
-			/>
-			<section className={style.sectionContent + ' ' + style.sectionDivisorOne}>
-				<TextContent
-					titulo='Facilitamos o fechamento de caixa e descomplicamos as tarefas financeiras do seu negócio.'
-					texto='Gestão financeira com foco na acessibilidade para pequenas empresas.'
-				/>
-				<Button>Quero experimentar</Button>
-			</section>
+  return (
+    <div className={style.mainContent} id={style[mode]}>
+      <Header />
+      <Carousel
+        imagesSrc={[
+          cnm_test_carousel,
+          cnm_test_carousel,
+          cnm_test_carousel,
+          cnm_test_carousel,
+          cnm_test_carousel,
+        ]}
+        options={{ autoplay: { enabled: true, delay: 2500 } }}
+      />
+      <section className={style.sectionContent + ' ' + style.sectionDivisorOne}>
+        <TextContent
+          titulo="Facilitamos o fechamento de caixa e descomplicamos as tarefas financeiras do seu negócio."
+          texto="Gestão financeira com foco na acessibilidade para pequenas empresas."
+        />
+        <Button>Quero experimentar</Button>
+      </section>
 
-			<section className={style.sectionDivisorTwo}>
-				<section className={style.sectionContent}>
-					<TextContent
-						titulo='Transforme Seu Processo Financeiro com Nossa Aplicação'
-						texto='Simplificamos o fechamento de caixa, permitindo que você e seus funcionários realizem as tarefas financeiras de forma rápida e sem complicações.'
-					/>
-				</section>
-				<Carousel
-					imagesSrc={[
-						cnm_test_carousel,
-						dark_logo,
-						light_logo,
-						cnm_gradient_arrow,
-						cnm_test_carousel,
-						dark_logo,
-						light_logo,
-						cnm_gradient_arrow,
-					]}
-					options={{ hasDots: true, autoplay: { enabled: true, delay: 2500 } }}
-				/>
-			</section>
-			<section
-				className={style.sectionContent + ' ' + style.sectionDivisorThree}
-			>
-				<div>
-					<img
-						src={cnm_gradient_arrow}
-						alt='cnm_gradient_arrow'
-						className={style.gradientArrow}
-					/>
-				</div>
-				<TextContent
-					titulo='Experimente Agora e Transforme Sua Gestão Financeira'
-					texto='Experimente nossa aplicação hoje mesmo e descubra como podemos simplificar o fechamento de caixa, proporcionando controle total, segurança dos dados e uma experiência que impulsiona o sucesso do seu negócio.'
-				/>
+      <section className={style.sectionDivisorTwo}>
+        <section className={style.sectionContent}>
+          <TextContent
+            titulo="Transforme Seu Processo Financeiro com Nossa Aplicação"
+            texto="Simplificamos o fechamento de caixa, permitindo que você e seus funcionários realizem as tarefas financeiras de forma rápida e sem complicações."
+          />
+        </section>
+        <Carousel
+          imagesSrc={[
+            cnm_test_carousel,
+            cnm_test_carousel,
+            cnm_test_carousel,
+            cnm_test_carousel,
+            cnm_test_carousel,
+          ]}
+          options={{ hasDots: true, autoplay: { enabled: true, delay: 2500 } }}
+        />
+      </section>
+      <section
+        className={style.sectionContent + ' ' + style.sectionDivisorThree}
+      >
+        <div>
+          <img
+            src={cnm_gradient_arrow}
+            alt="cnm_gradient_arrow"
+            className={style.gradientArrow}
+          />
+        </div>
+        <TextContent
+          titulo="Experimente Agora e Transforme Sua Gestão Financeira"
+          texto="Experimente nossa aplicação hoje mesmo e descubra como podemos simplificar o fechamento de caixa, proporcionando controle total, segurança dos dados e uma experiência que impulsiona o sucesso do seu negócio."
+        />
 
-				<Link
-					style={{ width: '100%', marginTop: 'var(--size-md)' }}
-					to={'/login'}
-				>
-					<Button>Login / Cadastro</Button>
-				</Link>
-			</section>
-			<Footer />
-		</div>
-	);
+        <Link
+          style={{ width: '100%', marginTop: 'var(--size-md)' }}
+          to={'/login'}
+        >
+          <Button>Login / Cadastro</Button>
+        </Link>
+      </section>
+      <Footer />
+    </div>
+  );
 };
 
 export default Home;
