3bf8427012ffd5c97de0742156411221b627eac2
1manuelc <manuelwn21@gmail.com>
2025-06-01 15:34:51 -0300

    refactor: atualização de importação de ícones Phosphor

--- src/views/components/fallback/Fallback.tsx
+++ src/views/components/fallback/Fallback.tsx
@@ -1,51 +1,50 @@
-import React from 'react';
-import { LinkBreak } from 'phosphor-react';
 import style from './Fallback.module.css';
 import { Link, useLocation } from 'react-router-dom';
 import { useMode } from '../../context/ModeContext';
+import { LinkBreakIcon } from '@phosphor-icons/react';
 
 const Fallback = () => {
-  const { mode } = useMode();
-  const { search } = useLocation();
-  const params = new URLSearchParams(search);
-  const errorMessage = params.get('error') || 'Unknown error';
+	const { mode } = useMode();
+	const { search } = useLocation();
+	const params = new URLSearchParams(search);
+	const errorMessage = params.get('error') || 'Unknown error';
 
-  /*teste*/
+	/*teste*/
 
-  return (
-    <div className={style.fallback} id={style[mode]}>
-      <LinkBreak
-        color={
-          mode === 'light'
-            ? 'var(--color-neutral-600)'
-            : 'var(--color-neutral-300)'
-        }
-        size={120}
-      />
-      <p className={style.p}>Ops... Página não encontrada.</p>
-      <span className={style.span}>
-        <p>{errorMessage}</p>
-      </span>
-      <Link
-        to={'/'}
-        style={
-          mode === 'light'
-            ? {
-                color: 'var(--color-green-900)',
-                fontSize: 'var(--size-2xs)',
-                textDecoration: 'underline',
-              }
-            : {
-                color: 'var(--color-green-300)',
-                fontSize: 'var(--size-2xs)',
-                textDecoration: 'underline',
-              }
-        }
-      >
-        Voltar para a página anterior
-      </Link>
-    </div>
-  );
+	return (
+		<div className={style.fallback} id={style[mode]}>
+			<LinkBreakIcon
+				color={
+					mode === 'light'
+						? 'var(--color-neutral-600)'
+						: 'var(--color-neutral-300)'
+				}
+				size={120}
+			/>
+			<p className={style.p}>Ops... Página não encontrada.</p>
+			<span className={style.span}>
+				<p>{errorMessage}</p>
+			</span>
+			<Link
+				to={'/'}
+				style={
+					mode === 'light'
+						? {
+								color: 'var(--color-green-900)',
+								fontSize: 'var(--size-2xs)',
+								textDecoration: 'underline',
+						  }
+						: {
+								color: 'var(--color-green-300)',
+								fontSize: 'var(--size-2xs)',
+								textDecoration: 'underline',
+						  }
+				}
+			>
+				Voltar para a página anterior
+			</Link>
+		</div>
+	);
 };
 
 export default Fallback;
--- src/views/components/header/Header.tsx
+++ src/views/components/header/Header.tsx
@@ -1,72 +1,69 @@
 import React from 'react';
-// Importando style css
 import style from './Header.module.css';
-// Importando imagens
 import cnm_logo_light from '../../assets/logos/light-theme-assets/cnm-logo-light.svg';
 import cnm_logo_dark from '../../assets/logos/dark-theme-assets/cnm-logo-dark.svg';
-import CircleHalf from 'phosphor-react/src/icons/CircleHalf';
-// Importando ferramentas para configuração de rotas
 import { Link } from 'react-router-dom';
 import { useMode } from '../../context/ModeContext';
+import { CircleHalfIcon } from '@phosphor-icons/react';
 
 function setColorApp(mode: string) {
-  const app: HTMLDivElement | null = document.querySelector('.app');
-  const themeColor: HTMLMetaElement | null = document.querySelector(
-    'meta[name="theme-color"]',
-  );
-  const themeColorApple: HTMLMetaElement | null = document.querySelector(
-    'meta[name="apple-mobile-web-app-status-bar-style"]',
-  );
+	const app: HTMLDivElement | null = document.querySelector('.app');
+	const themeColor: HTMLMetaElement | null = document.querySelector(
+		'meta[name="theme-color"]'
+	);
+	const themeColorApple: HTMLMetaElement | null = document.querySelector(
+		'meta[name="apple-mobile-web-app-status-bar-style"]'
+	);
 
-  if (app && themeColor && themeColorApple) {
-    mode === 'light'
-      ? (app.style.background = 'var(--color-neutral-100)') &&
-        (themeColor.content = 'var(--color-neutral-100)') &&
-        (themeColorApple.content = 'var(--color-neutral-100)')
-      : (app.style.background = 'var(--color-neutral-800)') &&
-        (themeColor.content = 'var(--color-neutral-800)') &&
-        (themeColorApple.content = 'var(--color-neutral-800)');
-  } else {
-    throw new Error('app não existe');
-  }
+	if (app && themeColor && themeColorApple) {
+		mode === 'light'
+			? (app.style.background = 'var(--color-neutral-100)') &&
+			  (themeColor.content = 'var(--color-neutral-100)') &&
+			  (themeColorApple.content = 'var(--color-neutral-100)')
+			: (app.style.background = 'var(--color-neutral-800)') &&
+			  (themeColor.content = 'var(--color-neutral-800)') &&
+			  (themeColorApple.content = 'var(--color-neutral-800)');
+	} else {
+		throw new Error('app não existe');
+	}
 }
 
 const Header = () => {
-  const { mode, setMode } = useMode();
+	const { mode, setMode } = useMode();
 
-  React.useEffect(() => {
-    localStorage?.setItem('mode', mode);
-    setColorApp(mode);
-  }, [mode]);
+	React.useEffect(() => {
+		localStorage?.setItem('mode', mode);
+		setColorApp(mode);
+	}, [mode]);
 
-  return (
-    <header className={style.header}>
-      <Link to="/">
-        <img
-          src={mode === 'light' ? cnm_logo_light : cnm_logo_dark}
-          alt={`logo-mode-${mode}`}
-          className={style.logoHeader}
-        />
-      </Link>
-      <nav className={style.nav}>
-        <button
-          onClick={() => {
-            setMode(mode === 'light' ? 'dark' : 'light');
-          }}
-        >
-          <CircleHalf
-            color={
-              mode === 'light'
-                ? 'var(--color-neutral-950)'
-                : 'var(--color-neutral-100)'
-            }
-            size={38}
-            weight="fill"
-          />
-        </button>
-      </nav>
-    </header>
-  );
+	return (
+		<header className={style.header}>
+			<Link to='/'>
+				<img
+					src={mode === 'light' ? cnm_logo_light : cnm_logo_dark}
+					alt={`logo-mode-${mode}`}
+					className={style.logoHeader}
+				/>
+			</Link>
+			<nav className={style.nav}>
+				<button
+					onClick={() => {
+						setMode(mode === 'light' ? 'dark' : 'light');
+					}}
+				>
+					<CircleHalfIcon
+						color={
+							mode === 'light'
+								? 'var(--color-neutral-950)'
+								: 'var(--color-neutral-100)'
+						}
+						size={38}
+						weight='fill'
+					/>
+				</button>
+			</nav>
+		</header>
+	);
 };
 
 export default Header;
