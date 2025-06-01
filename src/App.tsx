ef75f2d1641d8bd7cb8ba3bc9a9beee30314fcf3
1manuelc <manuelwn21@gmail.com>
2025-06-01 18:45:01 -0300

    refactor: remove div desnecessária abaixo da div#root

--- src/App.tsx
+++ src/App.tsx
@@ -5,29 +5,27 @@ import { ThemeContextProvider } from './views/context/ThemeContext';
 
 import { mainRoutes } from './routes/main-routes';
 
+// TODO: refatorar componentes que usam css modules para styled components
+
 function App() {
 	return (
-		<>
-			<div className='app'>
-				<BrowserRouter>
-					<ThemeContextProvider>
-						<ErrorBoundary>
-							<Routes>
-								{mainRoutes.map((route) => {
-									return (
-										<Route
-											path={route.path}
-											element={route.element}
-											key={route.path}
-										/>
-									);
-								})}
-							</Routes>
-						</ErrorBoundary>
-					</ThemeContextProvider>
-				</BrowserRouter>
-			</div>
-		</>
+		<BrowserRouter>
+			<ThemeContextProvider>
+				<ErrorBoundary>
+					<Routes>
+						{mainRoutes.map((route) => {
+							return (
+								<Route
+									path={route.path}
+									element={route.element}
+									key={route.path}
+								/>
+							);
+						})}
+					</Routes>
+				</ErrorBoundary>
+			</ThemeContextProvider>
+		</BrowserRouter>
 	);
 }
 
