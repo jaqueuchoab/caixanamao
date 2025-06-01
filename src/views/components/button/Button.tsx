478eec531e6c3d6ff7e3a707428296b1089306d1
1manuelc <manuelwn21@gmail.com>
2025-06-01 15:55:05 -0300

    refactor: children de Button como React.ReactNode

--- src/views/components/button/Button.tsx
+++ src/views/components/button/Button.tsx
@@ -1,21 +1,25 @@
-import React from 'react';
 import style from './Button.module.css';
 import { useMode } from '../../context/ModeContext';
 
 type ButtonProps = React.PropsWithChildren & {
-  children: string;
-  disabledButton?: boolean;
-  onClick?: () => unknown;
+	children: React.ReactNode;
+	disabledButton?: boolean;
+	onClick?: () => unknown;
 };
 
 const Button = ({ children, disabledButton, onClick }: ButtonProps) => {
-  const { mode } = useMode();
+	const { mode } = useMode();
 
-  return (
-    <button className={style.button} id={style[mode]} disabled={disabledButton} onClick={onClick}>
-      {children}
-    </button>
-  );
+	return (
+		<button
+			className={style.button}
+			id={style[mode]}
+			disabled={disabledButton}
+			onClick={onClick}
+		>
+			{children}
+		</button>
+	);
 };
 
 export default Button;
