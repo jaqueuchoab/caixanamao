9b0ee146417375e3c9c08b080e3b01aecd18c329
1manuelc <manuelwn21@gmail.com>
2025-07-09 17:05:35 -0300

    refactor: atualiza declaração de tipo de tema com interface

--- src/@types/default-styled-theme.d.ts
+++ src/@types/default-styled-theme.d.ts
@@ -5,5 +5,6 @@ import { ThemeType } from './theme-types';
  * com o tipo personalizado ThemeType, proporcionando autocomplete com a tipagem correta
  */
 declare module '@emotion/react' {
-	export type Theme = ThemeType;
+	// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+	interface Theme extends ThemeType {}
 }
