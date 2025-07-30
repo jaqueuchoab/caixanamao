92ebfc82d404c2073609246173ac505ad7be20f3
1manuelc <manuelwn21@gmail.com>
2025-07-30 11:32:42 -0300

    refactor: adiciona @test import alias do jest

--- jest.config.ts
+++ jest.config.ts
@@ -5,9 +5,12 @@ const config: Config = {
 	testEnvironment: 'jsdom',
 	moduleNameMapper: {
 		'\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+		'^@/(.*)$': '<rootDir>/src/$1',
+		'^@tests/(.*)$': '<rootDir>/src/tests/$1',
 	},
 	setupFilesAfterEnv: ['<rootDir>/src/tests/tests-setup.ts'],
 	testMatch: ['<rootDir>/src/**/*.test.{ts,tsx}'],
+	passWithNoTests: true,
 };
 
 export default config;
