f28ae398a178730f24b8d2148d6f8b4ec33555cd
1manuelc <manuelwn21@gmail.com>
2025-10-23 15:52:12 -0300

    feat(RegisterType): separa tipos do registro em editavel e padrao

--- src/models/registers/register.ts
+++ src/models/registers/register.ts
@@ -1,7 +1,18 @@
+export type EditableRegisterType = {
+	date: Date;
+	values: {
+		initial: number;
+		money: number;
+		creditCard: number;
+		pix: number;
+		expenses: number;
+	};
+};
+
 export type RegisterType = {
 	id: number;
-	startDate: number;
-	endDate: number;
+	startDate: Date;
+	endDate: Date;
 	values: {
 		initial: number;
 		money: number;
