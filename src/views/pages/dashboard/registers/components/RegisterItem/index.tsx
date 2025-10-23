ab31b7062dc5d1015dab5771752c89ce4e2e0bfb
1manuelc <manuelwn21@gmail.com>
2025-10-23 16:01:54 -0300

    feat(RegisterItem): implementa edicao de campo atraves de prop editable

--- src/views/pages/dashboard/registers/components/RegisterItem/index.tsx
+++ src/views/pages/dashboard/registers/components/RegisterItem/index.tsx
@@ -1,19 +1,27 @@
 import { Icon } from '@phosphor-icons/react';
 import { Container, Infos, Value } from './styles';
-import { HTMLAttributes } from 'react';
+import { HTMLAttributes, useId } from 'react';
+import Input from '@/views/components/ui/input/Input';
 
 interface RegisterItemProps extends HTMLAttributes<HTMLDivElement> {
 	name: string;
+	editable?: boolean;
 	value: number;
+	error?: string;
+	onChangeValue?: (value: number) => void;
 	icon: Icon;
 }
 
 export function RegisterItem({
 	name,
 	value,
+	onChangeValue,
+	error,
+	editable = false,
 	icon: IconComponent,
 	...rest
 }: RegisterItemProps) {
+	const id = useId();
 	return (
 		<Container {...rest}>
 			<Infos>
@@ -21,10 +29,23 @@ export function RegisterItem({
 				{name}
 			</Infos>
 			<Value>
-				<span className='registerItem__currency'>R$</span>
-				<span className='registerItem__value'>
-					{value.toFixed(2).replace('.', ',')}
-				</span>
+				<span className="registerItem__currency">R$</span>
+				{editable ? (
+					<Input
+						id={id}
+						value={value ?? 0}
+						onChange={(e) =>
+							onChangeValue?.(Number(e.target.value))
+						}
+						inputMode="decimal"
+						type="number"
+						error={error}
+					/>
+				) : (
+					<span className="registerItem__value">
+						{value.toFixed(2).replace('.', ',')}
+					</span>
+				)}
 			</Value>
 		</Container>
 	);
