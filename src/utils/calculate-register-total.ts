c95eaeb4c8b516277426e595df8f01fc9643dd21
1manuelc <manuelwn21@gmail.com>
2025-10-30 12:19:10 -0300

    fix(calculateRegisterTotal): adiciona EditableRegisterType como tipo possivel de argumento

--- src/utils/calculate-register-total.ts
+++ src/utils/calculate-register-total.ts
@@ -1,6 +1,11 @@
-import { RegisterType } from '@/models/registers/register';
+import {
+	EditableRegisterType,
+	RegisterType,
+} from '@/models/registers/register';
 
-export function calculateRegisterTotal(register: RegisterType) {
+export function calculateRegisterTotal(
+	register: RegisterType | EditableRegisterType,
+) {
 	const values = register.values;
 	return Object.entries(values).reduce((acc, [key, value]) => {
 		const num = Number(value) || 0;
