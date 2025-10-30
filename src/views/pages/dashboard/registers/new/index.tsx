47bf7beb47a920c9123728c53b59bf8bb536c41f
1manuelc <manuelwn21@gmail.com>
2025-10-30 12:11:43 -0300

    fix(CardFillStep): remove etapa de preenchimento de registro por etapas calculadas na pagina de novo registro

--- src/views/pages/dashboard/registers/new/index.tsx
+++ src/views/pages/dashboard/registers/new/index.tsx
@@ -3,12 +3,15 @@ import { Container, Content, Form, FormActions, Title } from './styles';
 import { useContextTheme } from '@context/ThemeContext';
 import { Button } from '@components/ui/button/Button';
 import useMultiStepForm from '@/views/hooks/useMultistepForm';
-import { useRegisterStepsContext } from '@/views/context/RegisterStepsContext';
 
 import { FormProvider, useForm } from 'react-hook-form';
 import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { toast } from 'sonner';
+import { differenceInDays } from 'date-fns';
+import { DateRangeStep } from './steps/DateRangeStep';
+import { EndSummaryStep } from './steps/EndSummaryStep';
+import { EditableRegisterCard } from '../components/EditableRegisterCard';
 
 const schema = z.object({
 	startDate: z.date(),
@@ -29,28 +32,31 @@ const schema = z.object({
 
 export type Schema = z.infer<typeof schema>;
 
-// TODO: corrigir submit
 export function NewRegisterPage() {
 	const { theme } = useContextTheme();
-	const { steps, daysDiff } = useRegisterStepsContext();
-	const {
-		currentStep,
-		isFirstStep,
-		isLastStep,
-		nextStep,
-		previousStep,
-		currentStepIndex,
-	} = useMultiStepForm({ steps });
 
 	const methods = useForm({
 		resolver: zodResolver(schema),
 		mode: 'onChange',
 	});
 
+	const startDate = methods.watch('startDate');
+	const endDate = methods.watch('endDate');
+	const daysDiff =
+		startDate && endDate ? differenceInDays(endDate, startDate) + 1 : 0;
+
+	const cardFillSteps = Array.from({ length: daysDiff }, (_, k) => {
+		return {
+			number: k + 1,
+			title: 'data ' + k,
+			step: <EditableRegisterCard key={`register-card-${k}`} id={k} />,
+		};
+	});
+
 	const handleValidateNextStep = () => {
 		if (daysDiff <= 0) {
 			toast.error('Insira um intervalo de datas válido', {
-				description: `O intervalo selecionado tem ${daysDiff} dias`,
+				description: `Pelo menos 1 dia`,
 			});
 			return;
 		}
@@ -62,14 +68,33 @@ export function NewRegisterPage() {
 		alert('registro criado:\n' + JSON.stringify(methods.getValues()));
 	};
 
+	const steps = [
+		{
+			number: 0,
+			title: 'Intervalo de Datas',
+			step: <DateRangeStep key="date-range-step" />,
+		},
+		...cardFillSteps,
+		{
+			number: 2,
+			title: 'Resumo da inserção',
+			step: <EndSummaryStep key="end-summary-step" />,
+		},
+	];
+
+	const {
+		currentStep,
+		isFirstStep,
+		isLastStep,
+		nextStep,
+		previousStep,
+		currentStepIndex,
+	} = useMultiStepForm({ steps });
+
 	return (
 		<Container>
 			<Title>
-				<Button
-					onClick={() => window.history.back()}
-					variant="neutral"
-					style={{ padding: 0 }}
-				>
+				<Button onClick={() => window.history.back()} variant="neutral">
 					<ArrowLeftIcon
 						size={24}
 						weight="bold"
@@ -93,22 +118,40 @@ export function NewRegisterPage() {
 								<Button
 									text_align="center"
 									onClick={previousStep}
+									type="button"
 									variant="neutral"
 								>
-									Voltar
+									{currentStepIndex === 1
+										? 'Cancelar'
+										: 'Voltar'}
 								</Button>
 							)}
 
-							<Button
-								onClick={handleValidateNextStep}
-								type={isLastStep ? 'submit' : 'button'}
-								text_align="center"
-								disabled={
-									false /* desativar se estiver enviando*/
-								}
-							>
-								{isLastStep ? 'Enviar' : 'Próximo'}
-							</Button>
+							{!isLastStep && (
+								<Button
+									onClick={handleValidateNextStep}
+									type="button"
+									text_align="center"
+									disabled={
+										false /* desativar se estiver enviando*/
+									}
+								>
+									Próximo
+								</Button>
+							)}
+
+							{isLastStep && (
+								<Button
+									onClick={handleValidateNextStep}
+									type="submit"
+									text_align="center"
+									disabled={
+										false /* desativar se estiver enviando*/
+									}
+								>
+									Enviar
+								</Button>
+							)}
 
 							{/* botão de submit fica no step EndSummary*/}
 						</FormActions>
