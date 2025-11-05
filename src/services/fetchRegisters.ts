18d433d75d9359acf8e8a7a8678ee8b4d41b648f
Manuel C. <manuelwn21@gmail.com>
2025-11-05 11:15:58 -0300

    feat(fetchRegister): refatora para conectar com a api convertendo o tipo dos dados

--- src/services/fetchRegisters.ts
+++ src/services/fetchRegisters.ts
@@ -1,55 +1,88 @@
+import { api } from '@/lib/api';
+import { toast } from 'sonner';
 import { RegisterType } from 'src/models/registers/register';
+
+type RegisterInApi = {
+	data: Date;
+	data_final: Date;
+	id: string;
+	iduser: string;
+	valor_cartao: number;
+	valor_despesas: number;
+	valor_especie: number;
+	valor_inicial: number;
+	valor_pix: number;
+};
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+async function fakeFetch(): Promise<RegisterType[] | []> {
+	// Simula uma chamada fetch com Promise e setTimeout
+	const data = await new Promise<RegisterType[]>((resolve) => {
+		setTimeout(() => {
+			resolve([
+				{
+					id: 1,
+					startDate: new Date(),
+					endDate: new Date(),
+					values: {
+						initial: 150,
+						money: 220,
+						creditCard: 0,
+						pix: 120,
+						expenses: 30,
+					},
+				},
+				{
+					id: 2,
+					startDate: new Date(),
+					endDate: new Date(),
+					values: {
+						initial: 0,
+						money: 42,
+						creditCard: 94.9,
+						pix: 0,
+						expenses: 200,
+					},
+				},
+				{
+					id: 3,
+					startDate: new Date(),
+					endDate: new Date(),
+					values: {
+						initial: 50,
+						money: 20,
+						creditCard: 200,
+						pix: 75,
+						expenses: 15,
+					},
+				},
+			]);
+		}, 1200);
+	});
+
+	return data;
+}
 
 export async function fetchRegisters(): Promise<RegisterType[] | []> {
 	try {
-		// Simula uma chamada fetch com Promise e setTimeout
-		const response = await new Promise<RegisterType[]>((resolve) => {
-			setTimeout(() => {
-				resolve([
-					{
-						id: 1,
-						startDate: new Date(),
-						endDate: new Date(),
-						values: {
-							initial: 150,
-							money: 220,
-							creditCard: 0,
-							pix: 120,
-							expenses: 30,
-						},
-					},
-					{
-						id: 2,
-						startDate: new Date(),
-						endDate: new Date(),
-						values: {
-							initial: 0,
-							money: 42,
-							creditCard: 94.9,
-							pix: 0,
-							expenses: 200,
-						},
-					},
-					{
-						id: 3,
-						startDate: new Date(),
-						endDate: new Date(),
-						values: {
-							initial: 50,
-							money: 20,
-							creditCard: 200,
-							pix: 75,
-							expenses: 15,
-						},
-					},
-				]);
-			}, 1200);
+		const response = await api.get('/registers');
+		const data = response.data.map((r: RegisterInApi) => {
+			return {
+				id: r.id,
+				startDate: r.data,
+				endDate: r.data_final,
+				values: {
+					initial: r.valor_inicial,
+					money: r.valor_especie,
+					creditCard: r.valor_cartao,
+					expenses: r.valor_despesas,
+					pix: r.valor_pix,
+				},
+			};
 		});
-
-		return response;
+		return data;
 	} catch (error) {
-		// somente para periodo de desenvolvimento
-		alert(error);
+		if (error instanceof Error) toast.error(error.message);
 		return [];
 	}
 }
